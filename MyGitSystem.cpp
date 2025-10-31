a4bf75c4d5badd1a415ea773c812f2595f18c672
Ilhanemreadak <dev.adak.ie@outlook.com>
2025-10-31 11:43:27 +0300

    Add init command method - Initialise repository folders and creating Head file to referance the head branch

--- MyGitSystem.cpp
+++ MyGitSystem.cpp
@@ -3,8 +3,11 @@
 
 #include <iostream>
 #include <filesystem>
+#include <fstream>
+#include <string>
 
 using namespace std;
+int mygit_init();
 
 int main(int argc, char* argv[])
 {
@@ -21,29 +24,47 @@ int main(int argc, char* argv[])
 	string firstCommand = argv[1];
 
 	if (firstCommand == "init") {
-		std::cout << "\n(En) Initiliazing mygit repository... \n (Tr) Git deposu baslatiliyor... \n";
-		std::filesystem::current_path();
-
-		std::cout << "(En) Current path: \n(Tr) Gecerli dizin: " << std::filesystem::current_path() << "\n";
-
-		std::filesystem::create_directory(".mygit");
-		std::filesystem::create_directory(".mygit/objects");
-		std::filesystem::create_directory(".mygit/refs");
-
-		std::cout << "(En) Repository initialized!\n(Tr) Git deposu baslatildi!\n";
-
-		return 0;
+		mygit_init();
 	}
 
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+int mygit_init() { // Initializes a mygit repository in the current directory
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+	std::cout << "\n(En) Initiliazing mygit repository... (Tr) Git deposu baslatiliyor... \n";
+
+	try {
+		std::filesystem::current_path();
+
+		std::cout << "(En) Current path: (Tr) Gecerli dizin: " << std::filesystem::current_path() << "\n";
+
+		std::filesystem::create_directory(".mygit");
+		std::filesystem::create_directory(".mygit/objects");
+		std::filesystem::create_directory(".mygit/refs");
+		std::filesystem::create_directory(".mygit/refs/heads");
+		std::filesystem::create_directory(".mygit/refs/tags");
+
+		std::ofstream headFile(".mygit/HEAD"); // This method authomatically creates the file if it does not exist
+
+		if (headFile.is_open()) {
+			headFile << "ref: refs/heads/main\n"; // This file points to the current branch and default is main
+			headFile.close();
+			std::cout << "(En) HEAD file created! (Tr) HEAD dosyasi olusturuldu!\n";
+		}
+		else {
+			std::cout << "(En) Error: Could not create HEAD file! (Tr) Hata: HEAD dosyasi olusturulamadi!\n";
+			return 1;
+		}
+
+		std::cout << "(En) Repository initialized! (Tr) Git deposu baslatildi!\n";
+	}
+	catch (const std::filesystem::filesystem_error) {
+		std::cout << "(En) Error: While creating git repos an file error accured! (Tr) Hata: Git deposu baslatilirken bir dosya hatasi alindi!\n";
+		return 1;
+	}
+	catch (...) {
+		std::cout << "(En) Error: Unknown error accured while creating git repo! (Tr) Hata: Git deposu baslatilirken bilinmeyen bir hata alindi!\n";
+		return 1;
+	}
+	return 0;
+}
