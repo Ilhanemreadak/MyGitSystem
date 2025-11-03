d4b84b89044df50d7e2f19e86bf4f628842bfa85
Ilhanemreadak <dev.adak.ie@outlook.com>
2025-11-03 14:10:33 +0300

    Add placeholder for hash-object method

--- MyGitSystem.cpp
+++ MyGitSystem.cpp
@@ -1,4 +1,4 @@
-// mygit.cpp : This file contains the 'main' function. Program execution begins and ends there.
+﻿// mygit.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
@@ -8,24 +8,43 @@
 
 using namespace std;
 int mygit_init();
+int mygit_hash_object(char* argv[]);
+int mygit_cat_file();
 
 int main(int argc, char* argv[])
 {
-    if (argc < 2) {
+    if (argc < 2)
+	{
 		std::cout << "\n(En) Warning: You need to give an argument!\n(Tr) Uyari: Bir komut girmelisiniz!\n";
-
 		return 1;
     }
 
-	for (int i = 0; i < argc; i++) {
-		std::cout << "Argument " << i << ": " << argv[i] << std::endl;
-	}
+	string fcommand = argv[1];
 
-	string firstCommand = argv[1];
-
-	if (firstCommand == "init") {
+	if (fcommand == "init")
+	{
 		mygit_init();
 	}
+	else if (fcommand == "list-args")
+	{
+		for (int i = 0; i < argc; i++) {
+			std::cout << "Argument " << i << ": " << argv[i] << "\n";
+		}
+	}
+	else if (fcommand == "cat-file")
+	{
+		std::cout << "cat-file command selected\n";
+	}
+	else if (fcommand == "hash-object")
+	{
+		mygit_hash_object(argv);
+	}
+	else
+	{
+		std::cout << "\n(En) Warning: Unknown command!\n(Tr) Uyari: Bilinmeyen komut!\n";
+		return 1;
+	}
+
 
 }
 
@@ -66,5 +85,57 @@ int mygit_init() { // Initializes a mygit repository in the current directory
 		std::cout << "(En) Error: Unknown error accured while creating git repo! (Tr) Hata: Git deposu baslatilirken bilinmeyen bir hata alindi!\n";
 		return 1;
 	}
+	return 0;
+}
+
+int mygit_hash_object(char* argv[]) { // Hashes a file and stores it in the objects directory
+
+	if (argv[2] == nullptr) {
+		std::cout << "\n(En) Usage: mygit hash-object [-t <type>] [-w] [--path=<file>}\n(Tr) Kullanım: mygit hash-object [-t <tip>] [-w] [--path=<tip>]\n";
+		std:cout << "(En) Types: commit,tree,blob and tag. Default: blob\n(Tr) Tipler: commit,tree,blob and tag. Varsayılan: blob\n";
+		return 1;
+	}
+
+	string commandOption = argv[2];
+
+	if (commandOption != "--help" && commandOption != "-w" && commandOption != "-t" && commandOption.find("--path=") == string::npos) {
+		std::cout << "\n(En) Warning: Unknown option!\n(Tr) Uyari: Bilinmeyen secenek!\n";
+		return 1;
+	}
+
+
+	if (commandOption == "--help")
+	{
+		std::cout << "\n(En) Usage: mygit hash-object [-t <type>] [-w] [--path=<file>}\n(Tr) Kullanim: mygit hash-object [-t <tip>] [-w] [--path=<tip>]\n";
+		std::cout << "(En) Types: commit,tree,blob and tag. Default: blob\n(Tr) Tipler: commit,tree,blob and tag. Varsayılan: blob\n";
+		return 0;
+	}
+	else if (commandOption.find("--path=") != string::npos)
+	{
+		string filePath = commandOption.substr(7); // Extract the file path from the argument
+		std::cout << "(En) File path: " << filePath << "\n(Tr) Dosya yolu: " << filePath << "\n";
+
+	}
+	else if (commandOption == "-t")
+	{
+		if (argv[3] == nullptr) {
+			std::cout << "\n(En) Warning: You need to give a type after -t option!\n(Tr) Uyari: -t seceneginden sonra bir tip girmeniz gerekiyor!\n";
+			return 1;
+		}
+		string typeOption = argv[3];
+		if (typeOption != "commit" && typeOption != "tree" && typeOption != "blob" && typeOption != "tag") {
+			std::cout << "\n(En) Warning: Unknown type!\n(Tr) Uyari: Bilinmeyen tip!\n";
+			return 1;
+		}
+		std::cout << "(En) Type option selected: " << typeOption << "\n(Tr) Tip secenegi secildi: " << typeOption << "\n";
+	}
+	else if (commandOption == "-w") {
+		std::cout << "(En) Write option selected\n(Tr) Yazma secenegi secildi\n";
+	}
+	std::cout << "hash-object command selected\n";
+	return 0;
+}
+
+int mygit_cat_file() { // Displays the content of a hashed object
 	return 0;
 }
---
+++ src/README.md
@@ -0,0 +1,28 @@
+# MyGitSystem
+
+MyGitSystem, bir dosya yönetimi ve sürüm kontrol sistemi uygulamasıdır. Bu proje, Visual Studio Code kullanılarak geliştirilmiştir.
+
+## Kurulum
+
+1. **Visual Studio Code** yükleyin.
+2. **Git** yükleyin.
+3. **Bash terminali** açın.
+4. **Proje klonlayın:** `git clone https://github.com/Ilhanemreadak/MyGitSystem.git`
+5. **Klonlanan projeye gidin:** `cd MyGitSystem`
+6. **Başlatmak için Visual Studio Code'yi açın:** `code .`
+
+## Kullanım
+
+### Komutlar
+
+- `git add .`: Değişiklikleri staging alanına alır.
+- `git commit -m "Commit mesajı"`: Staging alındaki değişiklikleri kaydeder.
+- `git push origin main`: Yerel değişiklikleri uzak depoya gönderir.
+
+### Yapılandırma
+
+Proje yapılandırması dosyası (`mygit/config.json`) içerir. Bu dosya, projenizin genel ayarlarını ve özelliklerini yönetmek için kullanılır.
+
+## Sorular
+
+Herhangi bir sorunuz veya destek almak isterseniz, lütfen [GitHub Issues](https://github.com/Ilhanemreadak/MyGitSystem/issues) bölümüne bakın.
